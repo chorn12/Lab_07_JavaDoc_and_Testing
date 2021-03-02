ff005706ce6f6bfdc0ca0a1adfa97d3227607f26
chorn12 <chorn12@student.cscc.edu>
2021-03-02 18:32:55 -0500

    Added JavaDoc comments and expanded RPSLSpockTest.class

---
+++ src/edu/cscc/RPSLSpock.java
@@ -0,0 +1,75 @@
+package edu.cscc;
+import java.util.Random;
+
+/**
+ * RPSLSpock class validates user input and generates computer choice using Random number between 0 and 4
+ * @author Christy Horn
+ */
+public class RPSLSpock {
+
+    static Random rand = new Random(System.currentTimeMillis());
+
+    public static final String ROCK = "rock";
+    public static final String PAPER = "paper";
+    public static final String SCISSORS = "scissors";
+    public static final String LIZARD = "lizard";
+    public static final String SPOCK = "spock";
+
+    /**
+     * Validates user input. Trims whitespace from user entry and ignores character case.
+     * @param pick Choice for the game entered by user.
+     * @return If valid, removes whitespace from user input. If invalid, returns error msg and prompts user until valid input entered.
+     */
+    public static boolean isValidPick(String pick) {
+        if (pick == null) {
+            return false;
+        }
+        pick = pick.trim();
+        return (ROCK.equalsIgnoreCase(pick) ||
+                PAPER.equalsIgnoreCase(pick) ||
+                SCISSORS.equalsIgnoreCase(pick) ||
+                LIZARD.equalsIgnoreCase(pick) ||
+                SPOCK.equalsIgnoreCase(pick));
+    }
+
+    /**
+     * Generates a random number between 0 and 4 for computer choice.
+     * @return Depending on random number, returns the String associated with that case.
+     */
+    public static String generatePick() {
+        String pick = null;
+        switch (rand.nextInt(5)) {
+            case 0:
+                pick = ROCK;
+                break;
+            case 1:
+                pick = PAPER;
+                break;
+            case 2:
+                pick = SCISSORS;
+                break;
+            case 3:
+                pick = LIZARD;
+                break;
+            case 4:
+                pick = SPOCK;
+                break;
+        }
+        return pick;
+    }
+
+    /**
+     * Checks for computer win by checking computer choice against human choice.
+     * @param c_pick Computer choice generated randomly.
+     * @param h_pick Human choice entered via user input.
+     * @return Compares Human and Computer choices and returns appropriate scenario.
+     */
+    public static boolean isComputerWin(String c_pick,String h_pick) {
+        h_pick = h_pick.toLowerCase();
+        return ((ROCK.equals(c_pick) && (SCISSORS.equals(h_pick) || LIZARD.equals(h_pick))) ||
+                (PAPER.equals(c_pick) && (ROCK.equals(h_pick) || SPOCK.equals(h_pick))) ||
+                (SCISSORS.equals(c_pick) && (PAPER.equals(h_pick) || LIZARD.equals(h_pick))) ||
+                (LIZARD.equals(c_pick) && (PAPER.equals(h_pick) || SPOCK.equals(h_pick))) ||
+                (SPOCK.equals(c_pick) && (ROCK.equals(h_pick) || SCISSORS.equals(h_pick))));
+    }
+}
